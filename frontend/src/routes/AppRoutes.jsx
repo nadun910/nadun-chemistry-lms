1dd388bbb2140a7c646e1f94b3279f77bc0b9dc7
nadun910 <nadunud@gmail.com>
2025-04-18 09:18:10 +0530

    update

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,7 +1,7 @@
 import { Container } from 'react-bootstrap';
 import { Routes, Route } from 'react-router-dom';
 import Navbar from '../components/layout/Navbar';
-import HomePage from '../pages/HomePage.JsX';
+import HomePage from '../pages/HomePage.jsx';
 import YearContentPage from '../pages/YearContentPage';
 
 export default function AppRoutes() {
