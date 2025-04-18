c51c42b92b15b5fdd6de88cb6d4b3e8adc8cc3c9
nadun910 <nadunud@gmail.com>
2025-04-18 18:04:50 +0530

    fff

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,9 +1,10 @@
 import { Routes, Route } from 'react-router-dom';
-import MainNavbar from '../components/layout/MainNavbar';
+import MainNavbar from '../components/layout/MaincdNavbar';
 import HomePage from '../pages/HomePage';
 import Login from '../pages/auth/Login';
 import YearContentPage from '../pages/YearContentPage';
 import { Container } from 'react-bootstrap';
+import AppRoutes from './routes/AppRoutes';
 
 export default function AppRoutes() {
   return (
