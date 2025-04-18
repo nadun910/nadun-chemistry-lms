8429675ba7e3641cabb0d9a34340461ec05538f6
nadun910 <nadunud@gmail.com>
2025-04-18 11:39:52 +0530

    gg

--- frontend/src/routes/AppRoutes.jsx
+++ frontend/src/routes/AppRoutes.jsx
@@ -1,5 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
-import MainNavbar from '../components/layout/Navbar';
+import MainNavbar from '../components/layout/MainNavbarNavbar';
 import HomePage from '../pages/HomePage';
 import Login from '../pages/auth/Login';
 import YearContentPage from '../pages/YearContentPage';
