5df1772bf9dc70733544cec9026203ddcdc218b4
nadun910 <nadunud@gmail.com>
2025-04-19 07:35:26 +0530

    hgg

--- frontend/src/components/layout/Navbar.jsx
+++ frontend/src/components/layout/Navbar.jsx
@@ -1,19 +1,24 @@
-import { Container, Nav, Navbar as BootstrapNavbar } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
-export default function Navbar() {
+export default function MainNavbar() {
   return (
-    <BootstrapNavbar bg="dark" variant="dark" expand="lg">
+    <Navbar bg="dark" variant="dark" expand="lg" sticky="top" collapseOnSelect>
       <Container>
-        <BootstrapNavbar.Brand as={Link} to="/">Nadun Chemistry</BootstrapNavbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link as={Link} to="/2025">2025 A/L</Nav.Link>
-          <Nav.Link as={Link} to="/2026">2026 A/L</Nav.Link>
-        </Nav>
-        <Nav>
-          <Nav.Link as={Link} to="/login">Login</Nav.Link>
-        </Nav>
+        <Navbar.Brand as={Link} to="/" className="fw-bold">
+          Nadun Chemistry
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/2025">2025 A/L</Nav.Link>
+            <Nav.Link as={Link} to="/2026">2026 A/L</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link as={Link} to="/login">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
-    </BootstrapNavbar>
+    </Navbar>
   );
 }
