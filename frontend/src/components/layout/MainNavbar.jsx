331df71cef23cbdadb28f1281c1d9bd9f1d46432
nadun910 <nadunud@gmail.com>
2025-04-19 07:59:33 +0530

    fdffd

--- frontend/src/components/layout/MainNavbar.jsx
+++ frontend/src/components/layout/MainNavbar.jsx
@@ -1,19 +1,30 @@
-import { Container, Nav, Navbar as BootstrapNavbar } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import './Navbar.css'; // We'll create this file next
 
 export default function MainNavbar() {
   return (
-    <BootstrapNavbar bg="dark" variant="dark" expand="lg">
-      <Container>
-        <BootstrapNavbar.Brand as={Link} to="/">Nadun Chemistry</BootstrapNavbar.Brand>
-        <Nav className="me-auto">
-          <Nav.Link as={Link} to="/2025">2025 A/L</Nav.Link>
-          <Nav.Link as={Link} to="/2026">2026 A/L</Nav.Link>
-        </Nav>
-        <Nav>
-          <Nav.Link as={Link} to="/login">Login</Nav.Link>
-        </Nav>
+    <Navbar 
+      bg="dark" 
+      variant="dark" 
+      expand="lg"
+      className="custom-navbar" // Added custom class
+    >
+      <Container fluid>
+        <Navbar.Brand as={Link} to="/" className="navbar-brand-custom">
+          Nadun Chemistry
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/2025" className="nav-link-custom">2025 A/L</Nav.Link>
+            <Nav.Link as={Link} to="/2026" className="nav-link-custom">2026 A/L</Nav.Link>
+          </Nav>
+          <Nav>
+            <Nav.Link as={Link} to="/login" className="nav-link-custom">Login</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
       </Container>
-    </BootstrapNavbar>
+    </Navbar>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';  // <-- ADD THIS LINE
+console.log('Bootstrap CSS loaded!');
 import App from './App';
 import './index.css';
 
